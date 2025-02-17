cafecfaab2730c1278f785a3f2b0dc8fa374c4db
James Ghosling <pachulisk@gmail.com>
2025-02-17 12:35:12 +0800

    update gw_login

--- app/utils.py
+++ app/utils.py
@@ -4,6 +4,11 @@ import platform    # For getting the operating system name
 import subprocess  # For executing a shell command
 import re
 import ipaddress
+from fastapi import HTTPException
+from .sdk import SDK
+
+from contextlib import contextmanager
+
 
 def is_valid_ipv4(ip):
     """
@@ -100,3 +105,24 @@ def setkv(type, id, key, value):
     else:
         response = supabase.table(TABLE_NAME).insert({"type": type, "key": key, "id": id, "value": value}).execute()
     return response
+
+@contextmanager
+async def gw_login(gwid):
+    print("==gw_login")
+    try:
+        gw = await get_gateway_by_id(gwid)
+        if gw is None or len(gw.data) == 0:
+            raise HTTPException(status_code=400, detail="gateway not found")
+        # get gateway username, password and address
+        username = gw.data[0].get('username')
+        password = gw.data[0].get('password')
+        address = gw.data[0].get('address')
+        sdk = SDK()
+        if sdk.login(address, username, password):
+            yield sdk
+        else:
+            raise HTTPException(status_code=401, detail="登录失败")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    finally:
+        sdk.logout()
