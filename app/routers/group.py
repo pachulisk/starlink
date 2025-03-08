11e021b490160c5f367fdad974b2076864320b4f
James Ghosling <pachulisk@gmail.com>
2025-03-08 11:03:23 +0800

    update groups

--- app/routers/group.py
+++ app/routers/group.py
@@ -3,7 +3,7 @@ from app.supabase import supabase, to_date
 import uuid
 from ..sdk import SDK
 from ..task import TaskRequest, run_single_task
-from ..utils import batch_update_gw_strategy, get_basic_rpc_result, gw_login, normalize_traffic, get_date_obj_from_str, get_start_of_month, get_end_of_month, get_date
+from ..utils import batch_update_gw_group, get_basic_rpc_result, gw_login, normalize_traffic, get_date_obj_from_str, get_start_of_month, get_end_of_month, get_date
 from pydantic import BaseModel
 from datetime import datetime
 import json
@@ -66,3 +66,14 @@ async def get_gw_group(query: GetGWGroupParam):
     gwid = query.gwid
     result = get_gw_group_impl(gwid)
     return { "data": result }
+
+class TestBatchSyncGroups(BaseModel):
+    gwid: str
+
+@group.post("/test_batch_sync_group", tags=["test"])
+async def test_batch_sync_group(query: TestBatchSyncGroups):
+    gwid = query.gwid
+    group_list = get_gw_group_impl(gwid)
+    print("group_list = ", group_list)
+    response = batch_update_gw_group(group_list)
+    return { "data": response }
