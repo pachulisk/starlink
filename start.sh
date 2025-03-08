f1b94f940dc687e8a465d41f7001ba7840a1cad6
James Ghosling <pachulisk@gmail.com>
2025-03-08 10:26:13 +0800

    update start script

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-set -euo pipefail
-# pushd ./routers
-celery -A celery_app worker --loglevel=info
+# set -euo pipefail
+# # pushd ./routers
+# celery -A celery_app worker --loglevel=info
+
+nohup fastapi run app/main.py &
 
 # popd
