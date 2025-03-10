4346a882f8cee32f1088ad58a9029dae3f1c914f
James Ghosling <pachulisk@gmail.com>
2025-03-10 14:17:27 +0800

    add list_user_with_groups

--- app/routers/group.py
+++ app/routers/group.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, HTTPException, BackgroundTasks
+import urllib
 from app.supabase import supabase, to_date
 import uuid
 from ..sdk import SDK
 from ..task import TaskRequest, run_single_task
-from ..utils import batch_update_gw_group, get_basic_rpc_result, gw_login, normalize_traffic, get_date_obj_from_str, get_start_of_month, get_end_of_month, get_date
+from ..utils import str_strip, batch_update_gw_group, get_basic_rpc_result, gw_login, normalize_traffic, get_date_obj_from_str, get_start_of_month, get_end_of_month, get_date
 from pydantic import BaseModel
 from datetime import datetime
 import json
@@ -77,3 +78,94 @@ async def test_batch_sync_group(query: TestBatchSyncGroups):
     print("group_list = ", group_list)
     response = batch_update_gw_group(group_list)
     return { "data": response }
+
+class ListUserWithGroupsQuery(BaseModel):
+    gwid: str
+
+
+def encode_username(username, useUrlEncode=True):
+    """
+    将格式为user1的用户名称，转化为'CN=user1,DC=wflocal'
+    """
+    # 1. 检测username是否为空，如果为空则返回空字符串
+    if username is None:
+        return ""
+    # 2. 拼接'CN=xxx,DC=wflocal'
+    target = f"CN={username},DC=wflocal"
+    # 3. 如果useUrlEncode为True，则返回urlencode以后的结果，否则直接返回：
+    if useUrlEncode:
+        return urllib.parse.quote(target)
+    else:
+        return target
+
+def decode_username(username):
+    """
+    将格式为'CN=user1,DC=wflocal'的用户名字格式化为user1
+    """
+    # 1. 检测username是否为空，如果为空则返空字符串
+    if username is None:
+        return ""
+    # 1.5 检测username是否为urlencode之后结果，如果是的话先urldecode
+    # 检测username中是否包含%
+    if '%' in username:
+        username = urllib.parse.unquote(username) 
+       
+    # 2. 检测username是否包含'CN='，如果不包含则返回username
+    if 'CN=' not in username:
+        return username
+    # 3. 使用split函数将username按照','分割成列表
+    parts = username.split(',')
+    # 4. 遍历列表，找到包含'CN='的项
+    for part in parts:
+        if 'CN=' in part:
+            # 5. 使用split函数将该项按照'='分割成列表，取第二项作为用户名字
+            username = part.split('=')[1]
+            return username
+    # 都没有匹配，返回空字符串
+    return ""
+
+
+
+# 获取所有的用户和分组
+@group.post("/list_user_with_groups", tags=["group"])
+async def list_user_with_groups(query: ListUserWithGroupsQuery):
+    """
+    列出网关下的所有组(group)，然后列出每一个组关联的用户
+    输入参数: query.gwid: 网关ID
+    返回值: 一个列表data, 每一项包括用户的id、用户所在组的id，用户所在组的名称
+    """
+    gwid = query.gwid
+    #1. 使用get_gw_group_impl获取已有的组信息
+    group_list = get_gw_group_impl(gwid)
+    #2. 设置返回值为data,一个空数组
+    data = []
+    # 对于group_list中每个组信息遍历
+    for group in group_list:
+        global_id = group.get("global_id")
+        alias = get_group_alias(group)
+        group_id = group.get("id")
+        # 打印debug信息
+        print(f"list_user_with_groups: Processing group: {global_id}, alias: {alias}, id: {id}")
+        # 使用list_virtual_group来获取组关联的用户
+        with gw_login(gwid) as sdk_obj:
+            result = sdk_obj.list_virtual_group(group_id)
+            r = get_basic_rpc_result(result)
+            r = str_strip(r.get("result"))
+            # 打印r的信息
+            print(f"list_user_with_groups: Users in group {global_id}: {r}")
+            # 获取users
+            users = r.get("users")
+            # 对于users数组中的每一个项遍历
+            for user in users:
+                userid = user.get("user")
+                userid = decode_username(userid)
+                u = {
+                    "userid": userid,
+                    "groupid": group_id,
+                    "group_global_id": global_id,
+                    "group_name": alias
+                }
+                data.append(u)
+    # 打印data的信息
+    print(f"list_user_with_groups: Final data: {data}")
+    return { "data": data }
