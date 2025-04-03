3059734008152cde0acc03ad29d43475a93feee1
James Ghosling <pachulisk@gmail.com>
2025-04-03 13:42:34 +0800

    add git status

--- app/routers/group.py
+++ app/routers/group.py
@@ -5,7 +5,7 @@ from app.supabase import supabase, to_date,str_strip
 import uuid
 from ..sdk import SDK
 from ..task import TaskRequest, run_single_task
-from ..utils import batch_update_users_group, batch_update_gw_group, get_basic_rpc_result, gw_login, normalize_traffic, get_date_obj_from_str, get_start_of_month, get_end_of_month, get_date
+from ..utils import is_empty, is_not_empty, batch_update_users_group, batch_update_gw_group, get_basic_rpc_result, gw_login, normalize_traffic, get_date_obj_from_str, get_start_of_month, get_end_of_month, get_date
 from pydantic import BaseModel
 from datetime import datetime
 import json
@@ -67,7 +67,27 @@ def get_gw_group_impl(gwid:str):
 @group.post("/get_gw_group", tags=["group"])
 async def get_gw_group(query: GetGWGroupParam):
     gwid = query.gwid
-    result = get_gw_group_impl(gwid)
+    result = []
+    if is_not_empty(gwid):
+        result = get_gw_group_impl(gwid)
+    else:
+        # 使用supabase获取所有的group和网关
+        r = supabase.table("gw_groups").select("*").execute()
+        for item in r.data:
+            val = {
+                "gwid": item.get("gwid"),
+                "id": item.get("id"),
+                "aliaz_en_us": item.get("aliaz_en_us"),
+                "alias_zh_cn": item.get("alias_zh_cn"),
+                "index": item.get("index"),
+                "anonymous": item.get("anonymous"),
+                "type": item.get("type"),
+                "alias": get_group_alias(item),
+                "virtual": get_virtual(item),
+                "global_id": f"{item.get("gwid")}_{item.get('id')}",
+                "name": item.get("name"),
+            }
+            result.append(val)
     return { "data": result }
 
 class TestBatchSyncGroups(BaseModel):
